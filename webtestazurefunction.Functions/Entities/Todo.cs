faf3ec0362d105f9f11e2d63ad19a191eb6790e5
Andrés Restrepo <jandru0512@gmail.com>
2021-08-23 17:13:54 -0500

    Add entity

---
+++ webtestazurefunction.Functions/Entities/Todo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webtestazurefunction.Functions.Entities
+{
+    public class Todo : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
