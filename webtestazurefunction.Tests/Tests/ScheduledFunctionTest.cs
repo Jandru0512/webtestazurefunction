375db3bc1b55ebb8305a5ffe2332c242d3f60e39
Andrés Restrepo <jandru0512@gmail.com>
2021-08-24 01:04:48 -0500

    Azure publish

---
+++ webtestazurefunction.Tests/Tests/ScheduledFunctionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using webtestazurefunction.Functions;
+using Xunit;
+
+namespace webtestazurefunction.Tests.Tests
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_log_Message()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            // Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains($"Deleting completed", message);
+        }
+    }
+}
